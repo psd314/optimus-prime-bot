37a3e90a75c9a3679faefc5cd899c4cd0e48e3a3
Philippe Dixon <pdixon555@hotmail.com>
2018-05-11 22:34:13 -0400

    initial commit

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=optimusprimebot-Web-Deploy.pubxml -p:Password=ghyDtRj0hmtZTjtFobQfZPZueqNuJ0exieFherfhnedbyJmPiHShjSHQ72NW
+
